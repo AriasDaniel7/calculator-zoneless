357bf4842d1957bdd3cf177fb2b50c8ee794f5b2
AriasDaniel7 <daniel7arias2003@gmail.com>
2025-07-26 18:01:30 -0500

    uso de host listener directo en el host + servicio de logica de calculadora

--- src/app/calculator/components/calculator-button/calculator-button.component.ts
+++ src/app/calculator/components/calculator-button/calculator-button.component.ts
@@ -1,4 +1,12 @@
-import { Component, HostBinding, input } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostBinding,
+  input,
+  output,
+  signal,
+  viewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'calculator-button',
@@ -7,10 +15,16 @@ import { Component, HostBinding, input } from '@angular/core';
   styleUrl: './calculator-button.component.css',
   host: {
     class: 'w-1/4 border-r border-b border-indigo-400',
+    '[class.w-2/4]': 'isDoubleSize()',
+    '[class.w-1/4]': '!isDoubleSize()',
   },
   // encapsulation: ViewEncapsulation.None,
 })
 export class CalculatorButtonComponent {
+  onClick = output<string>();
+  contentValue = viewChild<ElementRef<HTMLButtonElement>>('button');
+  isPressed = signal(false);
+
   isCommand = input(false, {
     transform: (value: boolean | string) =>
       typeof value === 'string' ? value === '' : value,
@@ -26,8 +40,33 @@ export class CalculatorButtonComponent {
       typeof value === 'string' ? value === '' : value,
   });
 
-  @HostBinding('class.w-2/4')
-  get commandStyle() {
-    return this.isDoubleSize();
+  // @HostBinding('class.w-2/4')
+  // get commandStyle() {
+  //   return this.isDoubleSize();
+  // }
+
+  handleClick() {
+    const button = this.contentValue()?.nativeElement;
+
+    if (!button) {
+      return;
+    }
+
+    this.onClick.emit(button.innerText.trim());
+  }
+
+  keyboardPressedStyle(key: string) {
+    const button = this.contentValue()?.nativeElement;
+    if (!button) return;
+
+    const value = button.innerText.trim();
+
+    if (value !== key) return;
+
+    this.isPressed.set(true);
+
+    setTimeout(() => {
+      this.isPressed.set(false);
+    }, 100);
   }
 }
